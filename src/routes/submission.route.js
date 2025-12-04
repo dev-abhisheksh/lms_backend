9aa57f9ff24d6852993d5fcc03ee7bca8a7917ab
Abhishek Sharma <abhisheksharmaazpm2@gmail.com>
2025-12-04 23:34:16 +0530

    Add getSingleSubmission router

--- src/routes/submission.route.js
+++ src/routes/submission.route.js
@@ -1,7 +1,7 @@
 import express from "express"
 import verifyJWT from "../middlewares/auth.midleware.js"
 import authorizeRoles from "../middlewares/role.middleware.js";
-import { createSubmission, deleteSubmission, getAllSubmissions, gradingSubissions, mySubmissions } from "../controllers/submission.controller.js";
+import { createSubmission, deleteSubmission, getAllSubmissions, getSingleSubmission, gradingSubissions, mySubmissions } from "../controllers/submission.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 const router = express.Router();
@@ -11,5 +11,6 @@ router.get("/submissions/:assignmentId", verifyJWT, authorizeRoles("admin", "tea
 router.post("/grade/:submissionId", verifyJWT, authorizeRoles("teacher", "admin"), gradingSubissions)
 router.patch("/delete/:submissionId", verifyJWT, authorizeRoles("admin", "student"), deleteSubmission)
 router.get("/my-submissions", verifyJWT, authorizeRoles("student"), mySubmissions)
+router.get("/submission/:submissionId", verifyJWT, authorizeRoles("admin", "teacher"), getSingleSubmission)
 
 export default router;
