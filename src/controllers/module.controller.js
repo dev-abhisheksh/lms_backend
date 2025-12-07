230c15b8025e9391e5a802f3079138b711768b73
Abhishek Sharma <abhisheksharmaazpm2@gmail.com>
2025-12-07 20:46:14 +0530

    Add getModuleById controller & router

--- src/controllers/module.controller.js
+++ src/controllers/module.controller.js
@@ -118,7 +118,77 @@ const getAllModules = async (req, res) => {
     }
 }
 
+const getModuleById = async (req, res) => {
+    try {
+        const { moduleId } = req.params;
+        if (!moduleId) return res.status(400).json({ message: "ModuleID is required" })
+
+        const module = await Module.findById(moduleId)
+            .populate({
+                path: "course",
+                select: "title isPublished",
+                populate: { path: "department", select: "name code isActive" }
+            })
+        if (!module) return res.status(404).json({ message: "module not found" })
+
+        const course = module?.course;
+        const department = module?.course?.department
+
+        if (req.user.role === "admin") {
+            return res.status(200).json({
+                message: "Module fetched successfully",
+                module
+            })
+        }
+
+        if (!department.isActive) return res.status(403).json({ message: "Department is not active" })
+        if (!module.isActive) return res.status(403).json({ message: "Module is deleted" })
+
+        if (req.user.role === "manager") {
+            if (!course.isPublished) return res.status(403).json({ message: "Course is not published" })
+            return res.status(200).json({
+                message: "Module fetched successfully",
+                module
+            })
+        }
+
+        //teacher validations
+        if (req.user.role === "teacher") {
+            const enrolledTeacher = await CourseEnrollment.findOne({
+                user: req.user._id,
+                course: course._id,
+                role: "teacher"
+            })
+            if (!enrolledTeacher) return res.status(403).json({ message: "You're not assigned to teach this course" })
+
+            return res.status(200).json({
+                message: "Module fetched successfully",
+                module
+            })
+        }
+
+        //student validations
+        if (!course.isPublished) return res.status(403).json({ message: "Course is not published" })
+        const enrolledStudent = await CourseEnrollment.findOne({
+            user: req.user._id,
+            course: course._id,
+            role: "student"
+        })
+        if (!enrolledStudent) return res.status(403).json({ message: "You're not enrolled in this course" })
+
+        return res.status(200).json({
+            message: "Module fetched successfully",
+            module
+        })
+
+    } catch (error) {
+        console.error("Failed to fetch Module", error)
+        return res.status(500).json({ message: "Failed to fetch Module" })
+    }
+}
+
 export {
     createModule,
-    getAllModules
+    getAllModules,
+    getModuleById
 }
--- src/routes/module.route.js
+++ src/routes/module.route.js
@@ -1,12 +1,13 @@
 import express from "express";
 import verifyJWT from "../middlewares/auth.midleware.js";
 import authorizeRoles from "../middlewares/role.middleware.js";
-import { createModule, getAllModules } from "../controllers/module.controller.js";
+import { createModule, getAllModules, getModuleById } from "../controllers/module.controller.js";
 
 
 const router = express.Router();
 
 router.post("/create/:courseId", verifyJWT, authorizeRoles("admin", "teacher"), createModule)
 router.get("/:courseId", verifyJWT, getAllModules)
+router.get("/module/:moduleId", verifyJWT, getModuleById)
 
 export default router;
