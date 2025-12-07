a8ce30a6582cc4f0291b93612de58ca6f4e7fa1f
Abhishek Sharma <abhisheksharmaazpm2@gmail.com>
2025-12-07 23:59:47 +0530

    Add deleteModule router

--- src/routes/module.route.js
+++ src/routes/module.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 import verifyJWT from "../middlewares/auth.midleware.js";
 import authorizeRoles from "../middlewares/role.middleware.js";
-import { createModule, getAllModules, getModuleById, toggleModule, updateModule } from "../controllers/module.controller.js";
+import { createModule, deleteModule, getAllModules, getModuleById, toggleModule, updateModule } from "../controllers/module.controller.js";
 
 
 const router = express.Router();
@@ -11,5 +11,6 @@ router.get("/:courseId", verifyJWT, getAllModules)
 router.get("/module/:moduleId", verifyJWT, getModuleById)
 router.patch("/update/:moduleId", verifyJWT, authorizeRoles("admin", "teacher"), updateModule)
 router.patch("/toggle/:moduleId", verifyJWT, authorizeRoles("admin"), toggleModule)
+router.delete("/delete/:moduleId", verifyJWT, authorizeRoles("admin", deleteModule))
 
 export default router;
