d0aa220850e48fc01da4586bc133e25e5068b853
Abhishek Sharma <abhisheksharmaazpm2@gmail.com>
2025-12-08 12:46:02 +0530

    Add getLessonsByModule controller & route

--- src/controllers/lesson.controller.js
+++ src/controllers/lesson.controller.js
@@ -2,6 +2,7 @@ import { Module } from "../models/module.model.js"
 import { CourseEnrollment } from "../models/courseEnrollment.model.js"
 import { uploadToCloudinary } from "../utils/cloudinary.js";
 import { Lesson } from "../models/lesson.model.js";
+import { Course } from "../models/course.model.js";
 
 const createLesson = async (req, res) => {
     try {
@@ -80,6 +81,103 @@ const createLesson = async (req, res) => {
     }
 }
 
+const getLessonsByModule = async (req, res) => {
+    try {
+        const { moduleId } = req.params;
+        if (!moduleId) return res.status(400).json({ message: "ModuleID is required" })
+
+        const module = await Module.findById(moduleId)
+            .populate({
+                path: "course",
+                select: "title isPublished",
+                populate: { path: "department", select: "name code isActive" }
+            })
+        if (!module) return res.status(404).json({ message: "Module not found" })
+
+        const course = module?.course;
+        const department = module?.course?.department
+
+        if (req.user.role === "admin") {
+            const lessons = await Lesson.find({ module: moduleId })
+
+            if (lessons.length === 0) return res.status(200).json({
+                message: "No lessons"
+            })
+
+            return res.status(200).json({
+                message: `All lessons fetched for module: ${module.title}`,
+                count: lessons.length,
+                lessons
+            })
+        }
+
+        if (!department.isActive) return res.status(403).json({ message: "Department is not active" })
+        if (!module.isActive) return res.status(403).json({ message: "Module is deleted" })
+
+        if (req.user.role === "manager") {
+            const lessons = await Lesson.find({ module: moduleId })
+
+            if (lessons.length === 0) return res.status(200).json({
+                message: "No lessons"
+            })
+
+            return res.status(200).json({
+                message: `All lessons fetched for module: ${module.title}`,
+                count: lessons.length,
+                lessons
+            })
+        }
+
+        if (req.user.role === "teacher") {
+            const enrolledTeacher = await CourseEnrollment.findOne({
+                user: req.user._id,
+                course: course._id,
+                role: "teacher"
+            })
+            if (!enrolledTeacher) return res.status(403).json({ message: "You're not assigned to teach this course" })
+
+            const lessons = await Lesson.find({ module: moduleId })
+
+            if (lessons.length === 0) return res.status(200).json({
+                message: "No lessons"
+            })
+
+            return res.status(200).json({
+                message: `All lessons fetched for module: ${module.title}`,
+                count: lessons.length,
+                lessons
+            })
+        }
+
+        if (!course.isPublished) return res.status(403).json({ message: "Course is not published" })
+
+        const enrolledStudent = await CourseEnrollment.findOne({
+            user: req.user._id,
+            course: course._id,
+            role: "student"
+        })
+
+        if (!enrolledStudent) return res.status(403).json({ message: "You're not enrolled in this course" })
+
+        const lessons = await Lesson.find({ module: moduleId, isActive: true })
+
+        if (lessons.length === 0) return res.status(200).json({
+            message: "No lessons"
+        })
+
+        return res.status(200).json({
+            message: `All lessons fetched for module: ${module.title}`,
+            count: lessons.length,
+            lessons
+        })
+
+    } catch (error) {
+        console.error("Failed to fetch lessons for module", error);
+        return res.status(500).json({ message: "Failed to fetch lessons for module" })
+    }
+}
+
 export {
-    createLesson
+    createLesson,
+    getLessonsByModule
 }
--- src/routes/lesson.route.js
+++ src/routes/lesson.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import verifyJWT from "../middlewares/auth.midleware.js";
 import authorizeRoles from "../middlewares/role.middleware.js";
-import { createLesson } from "../controllers/lesson.controller.js";
+import { createLesson, getLessonsByModule } from "../controllers/lesson.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 const router = express.Router();
 
 router.post("/create/:moduleId", verifyJWT, authorizeRoles("admin", "teacher"), upload.array("files", 5), createLesson)
+router.get("/:moduleId", verifyJWT, getLessonsByModule)
 
 export default router;
