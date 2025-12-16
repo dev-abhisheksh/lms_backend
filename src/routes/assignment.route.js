b9b851e97c7b4156b3228c8b71e095edb902420d
Abhishek Sharma <abhisheksharmaazpm2@gmail.com>
2025-12-16 23:57:36 +0530

    Add rate limiting to assignment creation and update routes to enhance API security

--- src/routes/assignment.route.js
+++ src/routes/assignment.route.js
@@ -3,11 +3,13 @@ import verifyJWT from "../middlewares/auth.midleware.js";
 import authorizeRoles from "../middlewares/role.middleware.js";
 import { createAssignment, deleteAssignment, getAssignmentByID, getAssignments, getAssignmentSummary, togglePublishUnpublishAssignment, updateAssignment } from "../controllers/assignment.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import rateLimiter from "../middlewares/rateLimiter.js";
 
 const router = express.Router();
 
-router.post("/create/:courseId", verifyJWT, authorizeRoles("admin", "teacher"), upload.array("attachments", 5), createAssignment)
-router.patch("/update/:assignmentId", verifyJWT, authorizeRoles("admin", "teacher"), upload.array("attachments", 5), updateAssignment)
+router.post("/create/:courseId", verifyJWT, authorizeRoles("admin", "teacher"), rateLimiter({ keyPrefix: "createAssignment", limit: 5, windowSec: 300 }), upload.array("attachments", 5), createAssignment)
+
+router.patch("/update/:assignmentId", verifyJWT, authorizeRoles("admin", "teacher"), rateLimiter({ keyPrefix: "updateAssignment", limit: 5, windowSec: 300 }), upload.array("attachments", 5), updateAssignment)
 router.get("/assignments/:courseId", verifyJWT, authorizeRoles("admin", "teacher", "student"), getAssignments)
 router.get("/assignment/:assignmentId", verifyJWT, getAssignmentByID)
 router.patch("/toggle/:assignmentId", verifyJWT, authorizeRoles("admin", "teacher"), togglePublishUnpublishAssignment)
