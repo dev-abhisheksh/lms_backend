24ff4b3db1e60cfcee84f19f36818e66011875ec
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-05-28 22:36:21 +0530

    feat: implement socket handlers for course joining and disconnection events

--- src/socketHandlers.js
+++ src/socketHandlers.js
@@ -1,1 +1,21 @@
-expor
+export const socketHandlers = (io) => {
+  io.on("connection", (socket) => {
+    console.log(`connected: ${socket.id}`)
+
+    //when students join assigned courses
+    socket.on("join-courses", (courseIds)=>{
+
+      if(!Array.isArray(courseIds)) return;
+
+      courseIds.forEach((id)=>{
+        socket.join(`course-${id}`)
+      })
+
+      console.log(`joined ${courseIds.length} courses/room`)
+    })
+
+    socket.on("disconnect", ()=>{
+      console.log(`disconnected ${socket.id}`)
+    })
+  })
+}
